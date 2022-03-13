1e355e51b7dcae7c70078bf71f8cb305754d928c
Fatma Osman <mademoisellebone@gmail.com>
2022-03-13 12:49:22 -0700

    uyt

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success)
+ * Returns: Always 0 (Success)
  */
 int main(void)
 	{
@@ -14,9 +14,9 @@ int main(void)
 			if (n != 57)
 			{
 				putchar(',');
-				putchar(',');
+				putchar(' ');
 			}
 		}
 		putchar('\n');
-		return (0);
+		return(0);
 	}
