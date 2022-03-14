6b8984959edb28fdedbab5fc08ed87bb243e395f
Fatma Osman <mademoisellebone@gmail.com>
2022-03-14 09:59:32 -0700

    last commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,14 +12,14 @@ int main(void)
 {
 	int i;
 	char l;
-	
+
 	i = 48;
 	while  (i < 58)
 	{
 		putchar(i);
 		i++;
 	}
-	
+
 	l = 'a';
 	while (l <= 'f')
 	{
@@ -27,6 +27,6 @@ int main(void)
 		l++;
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
