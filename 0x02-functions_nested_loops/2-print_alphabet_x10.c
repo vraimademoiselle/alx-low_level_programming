ca043e16f38d61acc767b593e95eaf9628f61606
Fatma Osman <mademoisellebone@gmail.com>
2022-03-15 13:09:07 -0700

    says no putchar c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+	char d;
+	int i = 0;
+
+	while (1 < 10)
+	{
+		d = 'a';
+		while (i < 10)
+		{
+			d = 'a';
+			while (d <= 'z')
+			{
+				_putchar(d);
+				d++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
