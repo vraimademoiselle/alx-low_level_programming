bfc96ef05be8b92c1a0ee988744f69012ba67e2c
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-17 22:22:00 +0300

    2nd commit for 1digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,20 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _isdigit(int c)- checks if its an number
- * 
- * @c: if its number return 1 else 0
- * Return: 1 if number else 0
+ * _isdigit - check for a digit
+ * @c : character to check
+ * Return:0 or 1
  */
+
 int _isdigit(int c)
 {
-    if ( c >= 48 && c <= 57)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	return (c >= 48 && c <= 57);
+
 }
