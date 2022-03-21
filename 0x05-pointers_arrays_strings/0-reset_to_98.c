dbd9cb9fdb1898396bd750b2f5cb9bd483416c6c
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-21 18:28:12 +0300

    fix bug

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * @brief 0-reset_to_98.c - takes a pointer and resets its value to 98
- * @version 0.1
+ * reset_to_98 - takes in pointer to an int and updates it to 98
  * @n: puntero
- * @date 2022-03-21
- * @copyright Copyright (c) 2022
  * Return: 0
  */
 
