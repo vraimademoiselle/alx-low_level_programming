50b4421e8f9a3155baea616ab0c66c00e48e295b
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-21 16:13:06 +0300

    commit error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,16 @@
-#include "main.h"                                                                                                                     
-#include <stdio.h>
-/*                                                                                                                                    
- * reset_to_98 - takes a pointer and resets its value to 98                                                                           
- *@n: The pointer                                                                                                                     
- * Return: Always 0.                                                                                                                  
+#include "main.h"
+
+/**
+ * @file 0-reset_to_98.c - takes a pointer and resets its value to 98
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-21
+ * 
+ * @copyright Copyright (c) 2022
+ * Return: 0
+ * 
  */
+
 void reset_to_98(int *n)
 {
     *n = 98;
