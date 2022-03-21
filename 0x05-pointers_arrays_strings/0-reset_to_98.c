36779f68907cb9f9848a35f4ecc32446478df2e1
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-21 16:18:15 +0300

    commit error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,11 @@
 
 /**
  * @file 0-reset_to_98.c - takes a pointer and resets its value to 98
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-03-21
- * 
  * @copyright Copyright (c) 2022
  * Return: 0
- * 
  */
 
 void reset_to_98(int *n)
