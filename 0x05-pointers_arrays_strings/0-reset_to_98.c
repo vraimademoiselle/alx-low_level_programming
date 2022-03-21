520b17e1093ec3eb1e7be5d7398c79f1fb7b1223
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-21 15:58:40 +0300

    fix error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,11 @@
  * reset_to_98 - takes a pointer and resets its value to 98                                                                           
  *@n: The pointer                                                                                                                     
  * Return: Always 0.                                                                                                                  
- */                                                                                                                                   
+ */   
+
 void reset_to_98(int *n)                                                                                                              
 {                                                                                                                                     
                                                                                                                                       
 *n = 98;                                                                                                                              
                                                                                                                                       
 }
-
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,4 @@
-#ifndef MAIN_H_
-#include <stdio.h>
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
