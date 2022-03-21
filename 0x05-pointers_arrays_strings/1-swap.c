4910dff6d88a3cc3fbd80c17df56f500f6be2f0b
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-21 17:51:14 +0300

    fixing compiling errors

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * swap_int - Updates the value of the parameter to 98
- * @a: The value
- * @b: The value
+ *swap_int - Updates the value of the parameter to 98
+ *@a: The value
+ *@b: The value
  *
  * Return: None
  */
