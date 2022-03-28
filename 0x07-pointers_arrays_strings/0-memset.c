6461c331adfb17978e56a78a07902ebf407a32a4
Fatma Osman <mademoisellebone@gmail.com>
2022-03-28 12:06:27 -0700

    fix it

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,17 +5,15 @@
  *@s: memory area to be filled
  *@b: char to copy
  *@n: number of times to copy b
- *
- *Return: pointer to the memory area s
+ *Return: ponter to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-
-return (s);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
