a9ae6ad74988a0c25168c0da857de0fbaf67dcee
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-30 23:44:37 +0300

    0 file commit of this dirtory

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- *_puts_recursion - function that prints a string
- *@s: charactor string
- *Return: 0
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: string
+ * Return: 0
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 if (*s != '\0')
 {
-
-_putchar(*s);
-_puts_recursion(s + 1);
+_print_rev_recursion(s + 1);
+printf("%c", *s);
 }
-else
-_putchar('\n');
 }
