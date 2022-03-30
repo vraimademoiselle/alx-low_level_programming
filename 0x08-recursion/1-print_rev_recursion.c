28a26c33db7f3418684f5943f098aab8ff7e358c
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-30 23:27:42 +0300

    0 file commit of this dirtory

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string
- * @s: charactor string
- * Return: 0
+ *_puts_recursion - function that prints a string
+ *@s: charactor string
+ *Return: 0
  */
 
 void _puts_recursion(char *s)
