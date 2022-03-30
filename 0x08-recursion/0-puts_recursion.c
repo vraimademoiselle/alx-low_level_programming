7a65c2f273e3eb173fe13914881862a1c764de29
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-30 23:16:10 +0300

    0 file commit of this dirtory

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string
- * @s: charactor string
- * Return: 0
- */
+*_puts_recursion -  prints a string, followed by a new line.
+*@s: string to print
+*/
 
 void _puts_recursion(char *s)
+
 {
-if (*s != '\0')
+if (*s == '\0')
 {
-putchar(*s);
-_puts_recursion(s + 1);
-}
-else
 putchar('\n');
+return;
+}
+putchar(*s);
+s++;
+_puts_recursion(s);
 }
