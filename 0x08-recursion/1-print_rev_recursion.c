a9f5153516e4cba98c8c3721b31797a544a89d1f
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-30 23:47:02 +0300

    0 file commit of this dirtory

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - function that prints a string in reverse
- * @s: string
- * Return: 0
+ *_print_rev_recursion - function that prints a string in reverse
+ *@s: string
+ *Return: 0
  */
 
 void _print_rev_recursion(char *s)
