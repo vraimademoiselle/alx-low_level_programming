782b9e637ba457da5cc5a0b8c329ba955a372667
vraimademoiselle <mademoisellebone@gmail.com>
2022-03-30 21:41:08 +0300

    8th file commit of this directory

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -29,7 +29,7 @@ int inception(char *s1, char *s2)
 		return (0);
 	if (*s1 == *s2)
 		ret += wildcmp(s1 + 1, s2 + 1);
-	ret += inception(s1 + 1, s2);
+	ret += wildcmp(s1 + 1, s2);
 	return (ret);
 }
 
