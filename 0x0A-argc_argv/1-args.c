217eaa92574869b131b212724bd621e31a9c5c4a
vraimademoiselle <mademoisellebone@gmail.com>
2022-04-01 12:55:39 +0300

    this is 1 file commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * @This program prints the number of arguments
  * 
  * @param argc - this counts the number of arguments 
  * @param argv - this allocates the storage for the said arguments.
- * @return int 
+ * @return
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc );
+	printf("%d\n", argc);
 	return (0);
 }
