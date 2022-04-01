8bc3dcdfd83b2406305b493a4eda28f7b79f8c85
Fatma Osman <mademoisellebone@gmail.com>
2022-04-01 08:21:56 -0700

    fix error

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * @This program prints the number of arguments
- * 
- * @param argc - this counts the number of arguments 
- * @param argv - this allocates the storage for the said arguments.
- * @return int 
+ *This program - prints the number of argument
+ *@param argc this counts the number of arguments
+ *@param argv this allocates the storage for the said arguments.
+ *@return int
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc );
+	printf("%d\n", argc);
 	return (0);
 }
