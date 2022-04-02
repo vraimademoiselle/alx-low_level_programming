4b1e29ea258c6a6d952cba6390f2c10e35024491
vraimademoiselle <mademoisellebone@gmail.com>
2022-04-02 23:52:49 +0300

    fixing error of file 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
-*main - print the number of arguments passed to the program
-*@argc: number of arguments
-*@argv: array of arguments
-*Return: Always 0 (Success)
+*main - Prints the number of arguments passed to it.
+*@argc: The number of arguments supplied to the program.
+*@argv: An array of pointers to the arguments.
+*Return: Always 0.
 */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%d\n", argc - 1);
+
 return (0);
 }
