78b68dc7d8437c34a61be2f51a2e96fb43a65ecd
vraimademoiselle <mademoisellebone@gmail.com>
2022-04-04 08:54:55 +0300

    commit 0 file now

---
+++ 0x08-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include <stdlib.h>
+/**
+ * main - This code uses malloc.
+ * @*ptr here is a pointer variable
+ * in order to execute malloc u need to use the prototpye -
+ * int *ptr = (int *)malloc(n*sizeof(int));
+ * then it finds the amount of adress needed to store an integer
+ * which is 4 bytes
+ */
+ int main()
+{
+     int i, n;
+     printf("Enter the number of integers: ");
+     scanf("%d", &n);
+     int *ptr = (int *)malloc(n*sizeof(int));
+     if(ptr == NULL)
+     {
+     printf("Memory not available.");
+     exit(1);
+    }
+    for(i=0; i<n; i++)
+    {
+        printf("Enter an integer: ");
+        scanf("%d", ptr +1);
+    }
+     for(i=0; i<n; i++)
+         printf("%d ", *(ptr+i));
+     return 0;
+     }
+     
