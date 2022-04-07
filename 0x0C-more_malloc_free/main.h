4e377e4e593ea4755e082c51c4422b61cca4066a
vraimademoiselle <mademoisellebone@gmail.com>
2022-04-07 16:20:51 +0300

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_HEADER_FILE
-#define Main_HEADER_FILE
+#define MAIN_HEADER_FILE
 
-#include <stdlib.h>
-int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+	#include <stdlib.h>
+	#include <stdio.h>
 
+	void *malloc_checked(unsigned int b);
+	char *string_nconcat(char *s1, char *s2, unsigned int n);
+	void *_calloc(unsigned int nmemb, unsigned int size);
+	int *array_range(int min, int max);
+	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+	int _putchar(char c);
 #endif
