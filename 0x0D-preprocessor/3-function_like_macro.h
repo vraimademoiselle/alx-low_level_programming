92929695567382f15bc0ca1189bbbfc8c39e3cd4
vraimademoiselle <mademoisellebone@gmail.com>
2022-04-11 15:05:10 +0300

    commiting 3rd file

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,16 @@
+#ifndef function_like_macro.h
+#define function_like_macro.h
+
+#define ABS(x)
+/**
+ *thus program computes the absolute value of x 
+ * x is the value
+ */
+int main(void)
+{
+    int x = 5;
+    printf("x = %d", x);
+
+    return(0);
+
+}
