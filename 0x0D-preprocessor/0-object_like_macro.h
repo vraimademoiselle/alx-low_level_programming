aa57cb764460756bb84c05aa0578969cafa8bf4c
Fatma Osman <mademoisellebone@gmail.com>
2022-04-11 05:40:02 -0700

    o file

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,19 @@
 #ifndef OBJECT_LIKE_MACRO_H
+
 #define OBJECT_LIKE_MACRO_H
 
-/*
- * File: 0-object_like_macro.h
+
+
+/**
+ *File: 0-object
  * Desc: Header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
+ *SIZE as an abbreviation for the token 1024.
  */
 
+
+
 #define SIZE 1024
+
+
 
 #endif
