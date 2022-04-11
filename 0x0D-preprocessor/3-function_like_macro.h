6bb07a787564a83f7f8272caf19474ef555d3e78
vraimademoiselle <mademoisellebone@gmail.com>
2022-04-11 15:16:04 +0300

    commiting 3rd file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef function_like_macro.h
-#define function_like_macro.h
+#ifndef _FUNTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
 
 #define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
 #endif
