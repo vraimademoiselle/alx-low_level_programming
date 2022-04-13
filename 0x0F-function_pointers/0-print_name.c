7b9ef7df929f75f4861a5e2b2eea6c5847aac741
vraimademoiselle <mademoisellebone@gmail.com>
2022-04-13 14:19:23 +0300

    changing directory name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,13 @@
 
 /** print_name is a function pointer that prints name
  * @name: name of the person
+ * f - pointer 
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 
  {
+    if name (NULL);
     (*f)(name);
      
  }
- 
