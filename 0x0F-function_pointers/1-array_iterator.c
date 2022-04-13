4e6e6831a8a9e2efe5dfd3ee04645be9d2f11d29
vraimademoiselle <mademoisellebone@gmail.com>
2022-04-13 15:37:23 +0300

    1 file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "function_pointer.h"
+
+/**
+ *@array: actual array
+ *@size: size of the array
+ *@action: is a pointer to the funtion 
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+    if (array == NULL || action == NULL)
+    return;
+
+    while (size > 0)
+    {
+        action(*array);
+        array++;
+    }
+}
