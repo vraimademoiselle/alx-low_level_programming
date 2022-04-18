0fd420359a9d4b0aad1073c26b21d284d742329d
Fatma Osman <mademoisellebone@gmail.com>
2022-04-18 00:13:36 -0700

    correction for 5printf file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/*8
- *main - Entry point
- *My hello, printf task 
- *Return: return 1 after program execution
+/**
+ *
+ * main - Program entry point
+ *
+ *Return: 0 Always
  */
+
 int main(void)
+
 {
-printf("with proper grammar, but the outcome is a piece of art,"\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
