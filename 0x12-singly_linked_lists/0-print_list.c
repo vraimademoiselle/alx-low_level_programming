fdbaa17ee3ab8f2476f4c0383917cf06adc97cae
vraimademoiselle <mademoisellebone@gmail.com>
2022-04-21 16:38:37 +0300

    101-hello

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t Fatma = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -24,5 +24,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 	}
 
-	return (Fatma);
+	return (nodes);
 }
